d3a3f69b47bdd8ba3809134c325e7a14225c7adc
ratnaraju <ratnaesi008@gmail.com>
2022-03-11 16:12:23 +0530

    HIS-134 fix provided

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
+	private static ConfigurableApplicationContext run;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		
+		//changes for HIS-134
+		
+		run.close();
 	}
 
 }
